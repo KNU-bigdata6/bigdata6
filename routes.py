f149290fe7f845547a997e96f55533ed2420fb30
dyd131001 <dyd131001@gmail.com>
2023-03-19 19:28:24 +0900

    flash

--- routes.py
+++ routes.py
@@ -1,37 +1,39 @@
-from flask import Flask, render_template, request, jsonify, make_response, Blueprint, redirect, url_for
+from flask import Flask, render_template, request, jsonify, make_response, Blueprint, redirect, url_for, flash
 from flask_login import login_user, logout_user, current_user
 from service.user import User
 import hashlib
 
-L = [["dasfad", "굿"], ["dasfad", "굿"], ["dasfad", "굿"]]
 
 main = Blueprint('main', __name__, url_prefix='/')
+
 
 @main.route('/')
 def vars():
     # runserver의 user_loader가 호출됨 flask login이 http request에서 id를 자동으로 넣어줌
     if current_user.is_authenticated:
-        print("로그인된 사용자")
-        print(current_user.name)
+        return render_template('home.html', userid=current_user.user_id, login=True)
     else:
-        print("안돼 돌아가")
-    return render_template('home.html')
+        return render_template('home.html')
 
 
 @main.route('/test', methods=['GET', 'POST'])
 def vars2():
-    if request.method == 'POST':
-        tdata = request.get_json()
-        # 데이터 db넣고 처리한후 ai 응답 담기
-        ai = {"data": "굿"}
-        L.append([tdata["question"], "굿"])
-        # ai db담기
-        return jsonify(result="success", result2=ai)
+    if current_user.is_authenticated:
+        if request.method == 'POST':
+            tdata = request.get_json()
+            # 데이터 db넣고 처리한후 ai 응답 담기
+            ai = {"data": "굿"}
+            # ai db담기
+            return jsonify(result="success", result2=ai)
+        else:
+            return render_template('test.html')
     else:
-        # 초기 세팅
-        return render_template('test.html', data_list=L)
+        # 경고문 띄우기
+        flash("not login")
+        return redirect(url_for('main.login'))
 
-@main.route('/join', methods = ['GET','POST'])
+
+@main.route('/join', methods=['GET', 'POST'])
 def join():
     if request.method == 'POST':
         user_id = request.form['userid']
@@ -39,8 +41,9 @@ def join():
         password = request.form['password']
         gender = "남" if request.form['gender'] == 'Male' else "여"
         hashed_password = hashlib.sha256(password.encode('utf-8')).hexdigest()
-        
-        SUCCESS = User.join(user_id = user_id , password = hashed_password, name = name, gender = gender)
+
+        SUCCESS = User.join(
+            user_id=user_id, password=hashed_password, name=name, gender=gender)
         # 가입 실패
         if not SUCCESS:
             return render_template('join.html')
@@ -57,20 +60,27 @@ def login():
         user_id = request.form['userid']
         password = request.form['password']
         hashed_password = hashlib.sha256(password.encode('utf-8')).hexdigest()
-        
+
         user = User.user_check(user_id, hashed_password)
         # 아이디나 비밀번호가 틀린 경우
-        if not user :
+        if not user:
+            #경고문 띄우기
+            flash("login failed")
             return render_template('login.html')
         else:
             print(user.user_id, user.password, user.name, user.gender)
             login_user(user)
             return redirect(url_for('main.vars'))
     else:
-        return render_template('login.html')
+        if current_user.is_authenticated:
+            return redirect(url_for('main.vars'))
+        else:
+            return render_template('login.html')
 
 # GET 메소드는 임시로 logout 버튼이 없어서 만들어 놓은것
-@main.route('/logout', methods = ['GET','POST'])
+
+
+@main.route('/logout', methods=['GET', 'POST'])
 def logout():
     logout_user()
     return redirect(url_for('main.vars'))
